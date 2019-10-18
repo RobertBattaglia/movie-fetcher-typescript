ac688c4146d29b40d9faefe766160eb04493b1a4
Robert Battaglia <r083rt164@gmail.com>
2019-10-18 00:18:47 -0400

    query api for all pages if more than 1 page

--- server/index.js
+++ server/index.js
@@ -11,8 +11,24 @@ app.get('/movie', async (request, response) => {
   try {
     const movie = request.query.movie;
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movie}&language=en-US&page=1&include_adult=true`
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movie}&language=en-US`
     );
+
+    if (data.total_pages > 1) {
+      const allPageData = [];
+      for (let i = 2; i <= data.total_pages; i++) {
+        allPageData.push(
+          axios.get(
+            `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movie}&page=${i}&language=en-US`
+          )
+        );
+      }
+
+      let pageResponses = await Promise.all(allPageData);
+      pageResponses = pageResponses.map(response => response.data.results);
+      data.results = data.results.concat(...pageResponses);
+    }
+
     response.json(data);
   } catch (error) {
     console.error(error);
